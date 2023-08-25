1487cdd2c6f3fa7063322168532d3c47540a7626
anssame <elgh@gmail.com>
2023-08-25 08:31:56 -0700

    stract

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,27 @@
 #include "main.h" 
-/*
+/**
  *@dest:pointers to destination input
  *@src:pointer to source input 
  *
  *return: pointer to resulting string @dest
-*/ 
+ */ 
 char *_strcat(char *dest, char *src)
-{
-	int c, c2;
+	int i;
+	int j;
 
-	c = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-		c++;
-
-		/*iterate through eachsrc array without the null bytes*/
-		for (c2 = 0; src[c2] ; c2++)
-		/*apend srcc2]to dest [c] while overwriting the null byte in dest*/
-		dest [c++] = src[c2];
-		return (dest);
-		}
+	i = 0;
+	while (dest[i] != '/0')
+	{
+		i++
+	}
+	j = 0;
+	while (src[j] != '/0'
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '/0';
+	return (dest)
+	
+	}
