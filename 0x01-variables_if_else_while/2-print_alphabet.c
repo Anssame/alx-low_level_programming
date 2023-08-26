7c0e976a92659041598cd35009c37006e25e362e
anssame <elgh@gmail.com>
2023-08-26 15:15:44 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits
+ * main - Prints the alphabet.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = 'a';
-	while (ch <- 'z')
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
-		putchar(ch):
-		ch++;
+		putchar(alp[i]);
 	}
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
