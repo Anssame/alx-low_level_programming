392cba3db029008064717cfe999cdefb7652b23e
anssame <elgh@gmail.com>
2023-08-26 14:57:10 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,24 +6,12 @@
  */
 int main(void)
 {
-	int n, m;
-
-	for (n = 48; n <= 56; n++)
+	char ch = 'a';
+	while (ch <- 'z')
 	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(ch):
+		ch++;
 	}
-	putchar('\n');
+	putchar('\n');	
 	return (0);
 }
