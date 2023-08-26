5e0a8fdd867b50a6238f66b7039e5601e20c993e
anssame <elgh@gmail.com>
2023-08-26 11:45:28 -0700

    quota

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<unistd.h>
 /**
  * main - entry point
@@ -9,8 +10,6 @@
  */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1. quo. 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout";
 	return (1);
 }
